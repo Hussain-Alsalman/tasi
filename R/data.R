bd90e113567e5dbf505cb5bac2b47cbb342e7152
Hussain Alsalman <Alsalman.hn@gmail.com>
2021-03-19 19:57:35 +0300

    Dataset Enhancement and Documentation

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' Stocks Information Dataset
+#'
+#' A dataset containing the stock symbols and other attributes of over 200 companies on Tadawul website.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 212 rows and 8 variables:
+#' \describe{
+#'   \item{symbol}{Company Symbol aka:Ticker e.g.("8220","4002" ..etc)}
+#'   \item{companyURL}{Company profile URL e.g.("/wps/portal/tadawul/markets/equities/indices/today/!ut/ ..")}
+#'   \item{sectorName}{Sector name to which companies belog e.g.("Bank","Energy" ..etc)}
+#'   \item{sectorURL}{Sector URL e.g.("/wps/portal/tadawul/markets/equities/indices/today/!...etc")}
+#'   \item{companyNameEN}{Full company name in English e.g.("Weqaya Takaful Insurance and Reinsurance Co.")}
+#'   \item{companyNameAR}{Full company name in Arabic e.g.("شركة وقاية للتأمين وإعادة التأمين التكافلي")}
+#'   \item{companyName}{Company name in English e.g.("Weqaya Takaful Insurance and Reinsurance Co.")}
+#'   \item{market_type}{Market type to differentiate it from Bond Market. Here, it is always "M" here}
+#'   \item{tradingNameEn}{Short company name in English e.g.("Weqaya Takaful")}
+#'   \item{tradingNameAr}{Short company name in Arabic e.g.("وقاية للتكافل")}
+#'   \item{isin}{System code, usually not used}
+#' }
+"stock_indices"
+
+
+#' Bond Information Dataset
+#'
+#' A dataset containing the bond symbols and other attributes of over 73 bonds on Tadawul website.
+#'  The variables are as follows:
+#'
+#' @format A data frame with 212 rows and 8 variables:
+#' \describe{
+#'   \item{symbol}{bond Symbol aka:Ticker e.g.("2315", "4035" ..etc)}
+#'   \item{companyNameEN}{Full bond name in English e.g.("Sadara Sukuk", "Bahri Sukuk")}
+#'   \item{companyNameAR}{Full bond name in Arabic e.g.("صكوك ساتورب" ,"صكوك كهرباء السعودية 3")}
+#'   \item{companyName}{Bond name in English e.g.("Sadara Sukuk" "Bahri Sukuk")}
+#'   \item{market_type}{Market type to differentiate it from Stock Market}
+#'   \item{isin}{System code, usually not used}
+#'   \item{bond_type}{Bondy type. "S" for private, "G" for government}
+#' }
+"bonds_indices"
--- data-raw/indices.R
+++ data-raw/indices.R
@@ -1,6 +1,7 @@
 # API endpoint for listing all stocks
 library("dplyr")
 library("readr")
+
 #Obtain all Data
 all_symbols<-rjson::fromJSON(file = "https://www.tadawul.com.sa/tadawul.eportal.theme.helper/ThemeSearchUtilityServlet")
 
@@ -17,9 +18,19 @@ for ( i in seq_along(col_names)){
     ifelse(!is.null(unlist(el[col_names[i]])), unlist(el[col_names[i]]),NA)})
 }
 
+# Obtain supplemental information for each company
+df_supplement <- rjson::fromJSON(file = "https://www.tadawul.com.sa/wps/portal/tadawul/markets/equities/indices/today/!ut/p/z1/rZLRTsIwFIafxYteSs9gAfVuIWHWTBICw9mbpesaqdnapT04eHsHdyZSNHrumvN9af6_pZwWlBvxod8EamtEM5xf-bQcj-d30X0MGWSzCJJpCmz1HE_SBdCXEABpRPmv_JQtZ5CsksftYrsZ_MnffIh_5sOFSeC6z4PIMgoD54q-At90EAROIc9AIMVaGfp0Lcvw2DvE7oEAgb7vR7pqR9K2BA5tYzyBztmaQC1Q4LFTBKQ1qAwScMrbvZPqVtqmUfL0dzzlKCpmanWgRaY9qnpu204Yrfx_XyR3wmGJwutS7p0byNIPS-tosUnWjHZtnucFaPYe8-rY33wCjfF_BA!!/p0/IZ7_NHLCH082KGN530A68FC4AN2OE0=CZ6_22C81940L0L710A6G0IQM43GF0=NEchart_tasi_current_sector!TASI==/!?_=1616168200088")
+df_supplement <- t(sapply(df_supplement$data, function(x) unlist(x)))
+df_supplement <- as.data.frame(df_supplement)
+
 # Split Data into Different Subsets
-stock_indices <- df %>% filter(market_type == "M") %>%  select(-bond_type)
+stock_indices_partial <- df %>% filter(market_type == "M") %>%  select(-bond_type)
 bonds_indices <- df %>% filter(market_type == "B") %>% select(-tradingNameEn, -tradingNameAr)
 
+
+stock_indices <- df_supplement %>%
+  select(-companyName) %>%
+  left_join(stock_indices_partial, by = "symbol")
+
 #write in the Stock market Data
-usethis::use_data(stock_indices, bonds_indices)
+usethis::use_data(stock_indices, bonds_indices, overwrite = TRUE)
