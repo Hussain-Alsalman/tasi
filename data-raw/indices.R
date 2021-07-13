bcb358387c11c05f605933c2192b0e56b0457920
Hussain Alsalman <Alsalman.hn@gmail.com>
2021-07-13 19:34:05 +0300

    Switching to tadawul new domain

--- data-raw/indices.R
+++ data-raw/indices.R
@@ -3,7 +3,7 @@ library("dplyr")
 library("readr")
 
 #Obtain all Data
-all_symbols<-rjson::fromJSON(file = "https://www.tadawul.com.sa/tadawul.eportal.theme.helper/ThemeSearchUtilityServlet")
+all_symbols<-rjson::fromJSON(file = "https://www.saudiexchange.sa/tadawul.eportal.theme.helper/ThemeSearchUtilityServlet")
 
 # Extract Column Names
 col_names <- unique(unlist(sapply(all_symbols, function(x) names(x))))
@@ -19,7 +19,7 @@ for ( i in seq_along(col_names)){
 }
 
 # Obtain supplemental information for each company
-df_supplement <- rjson::fromJSON(file = "https://www.tadawul.com.sa/wps/portal/tadawul/markets/equities/indices/today/!ut/p/z1/rZLRTsIwFIafxYteSs9gAfVuIWHWTBICw9mbpesaqdnapT04eHsHdyZSNHrumvN9af6_pZwWlBvxod8EamtEM5xf-bQcj-d30X0MGWSzCJJpCmz1HE_SBdCXEABpRPmv_JQtZ5CsksftYrsZ_MnffIh_5sOFSeC6z4PIMgoD54q-At90EAROIc9AIMVaGfp0Lcvw2DvE7oEAgb7vR7pqR9K2BA5tYzyBztmaQC1Q4LFTBKQ1qAwScMrbvZPqVtqmUfL0dzzlKCpmanWgRaY9qnpu204Yrfx_XyR3wmGJwutS7p0byNIPS-tosUnWjHZtnucFaPYe8-rY33wCjfF_BA!!/p0/IZ7_NHLCH082KGN530A68FC4AN2OE0=CZ6_22C81940L0L710A6G0IQM43GF0=NEchart_tasi_current_sector!TASI==/!?_=1616168200088")
+df_supplement <- rjson::fromJSON(file = "https://www.saudiexchange.sa/wps/portal/tadawul/markets/equities/indices/today/!ut/p/z1/rZLRTsIwFIafxYteSs9gAfVuIWHWTBICw9mbpesaqdnapT04eHsHdyZSNHrumvN9af6_pZwWlBvxod8EamtEM5xf-bQcj-d30X0MGWSzCJJpCmz1HE_SBdCXEABpRPmv_JQtZ5CsksftYrsZ_MnffIh_5sOFSeC6z4PIMgoD54q-At90EAROIc9AIMVaGfp0Lcvw2DvE7oEAgb7vR7pqR9K2BA5tYzyBztmaQC1Q4LFTBKQ1qAwScMrbvZPqVtqmUfL0dzzlKCpmanWgRaY9qnpu204Yrfx_XyR3wmGJwutS7p0byNIPS-tosUnWjHZtnucFaPYe8-rY33wCjfF_BA!!/p0/IZ7_NHLCH082KGN530A68FC4AN2OE0=CZ6_22C81940L0L710A6G0IQM43GF0=NEchart_tasi_current_sector!TASI==/!?_=1616168200088")
 df_supplement <- t(sapply(df_supplement$data, function(x) unlist(x)))
 df_supplement <- as.data.frame(df_supplement)
 
