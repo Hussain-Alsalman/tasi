cdcfa4e93694049ef9ff5d9295391ac38ec051dc
Hussain Alsalman <Alsalman.hn@gmail.com>
2022-10-12 21:12:59 +0300

    fixing few bugs

--- R/market_stats.R
+++ R/market_stats.R
@@ -19,8 +19,8 @@ colnames(tbl) <- cl_col_names
 tbl <- tbl[-1,]
 tbl <- tbl %>%
   dplyr::mutate(percent_change = stringr::str_remove_all(percent_change,pattern = "%")) %>%
-  dplyr::mutate(dplyr::across(.cols = 2:9, .fns = num_format)) %>%
-  dplyr::mutate(company = stringr::str_trim(str_remove_all(company, pattern = "\\*")))
+  dplyr::mutate(dplyr::across(.cols = 2:10, .fns = num_format)) %>%
+  dplyr::mutate(company = stringr::str_trim(stringr::str_remove_all(company, pattern = "\\*")))
 
 tbl <- tbl %>%
   dplyr::left_join(tasi::stock_indices, by = c("company" = "tradingNameEn"))
