862349d479b39640530126c8e1ab3a4369b4395f
Hussain Alsalman <Alsalman.hn@gmail.com>
2022-11-12 18:39:11 +0300

    fixing few typoes and updating stocks list

--- R/market_stats.R
+++ R/market_stats.R
@@ -1,11 +1,11 @@
 #' Getting daily market daily report
 #'
 #' @return data frame containing all daily report for all companies listed in Saudi Stock Market
+#' @import magrittr
 #' @export
 #'
 #' @examples
 #' get_daily_market_stats()
-#'
 get_daily_market_stats <- function() {
 
 url <- constants$daily_report
@@ -19,8 +19,6 @@ cl_col_names <- tbl[1, ] %>%
 colnames(tbl) <- cl_col_names
 tbl <- tbl[-1, ]
 tbl[,2:ncol(tbl)] <- apply(tbl[,-1], c(1,2), tasi:::num_format)
-
-
 tbl <- tbl %>%
   dplyr::left_join(tasi::stock_indices, by = c("company" = "tradingNameEn"))
 return(tbl)
